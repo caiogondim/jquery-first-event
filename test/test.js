847f5a075b34c02af0840db5d08601a861f26b15
Caio Gondim <me@caiogondim.com>
2015-09-05 15:52:54 +0200

    First tests

--- test/test.js
+++ test/test.js
@@ -2,6 +2,58 @@
 
 var assert = chai.assert
 
-it('should', function() {
-    assert(1 === 2, '1 equal 2')
+beforeEach(function() {
+    $('body *').remove()
+})
+
+it('should fire callback with `firstOn` before others', function() {
+    var eventsOrder = []
+
+    $('<p>').appendTo('body')
+
+    $('p')
+        .on('click', function() {
+            eventsOrder.push('vanilla 1')
+        })
+        .on('click', function() {
+            eventsOrder.push('vanilla 2')
+        })
+        .firstOn('click', function() {
+            eventsOrder.push('first event')
+        })
+        .trigger('click')
+
+    assert.deepEqual(eventsOrder, ['first event', 'vanilla 1', 'vanilla 2'])
+})
+
+it('should work with multiple selector', function() {
+    var eventsOrder = []
+
+    $('<p>').appendTo('body')
+    $('<div>').appendTo('body')
+    $('<span>').appendTo('body')
+
+    $('p, div, span')
+        .on('click', function() {
+            eventsOrder.push('vanilla 1')
+        })
+        .on('click', function() {
+            eventsOrder.push('vanilla 2')
+        })
+        .firstOn('click', function() {
+            eventsOrder.push('first event')
+        })
+        .trigger('click')
+
+    assert.deepEqual(eventsOrder, [
+        'first event',
+        'vanilla 1',
+        'vanilla 2',
+        'first event',
+        'vanilla 1',
+        'vanilla 2',
+        'first event',
+        'vanilla 1',
+        'vanilla 2',
+    ])
 })
